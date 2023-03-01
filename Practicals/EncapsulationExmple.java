47118915c885c1b5f31478bf64825a4cf68ddfb1
narendra-khadke <99339951+narendra-khadke@users.noreply.github.com>
2023-03-01 03:15:59 -0800

    Add files via upload

---
+++ Practicals/EncapsulationExmple.java
@@ -0,0 +1,20 @@
+package CoreJava;
+class EncapsulationExmple {
+	//Fields to calculate area
+	private int id=10;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Practicals/TestEncapsu.java
@@ -0,0 +1,15 @@
+package CoreJava;
+public class TestEncapsu {
+
+	public static void main(String[] args) {
+		
+		// Creating object of class EncapsulationExample
+		EncapsulationExmple e1=new EncapsulationExmple();
+		//Object is use to access the methods of the class
+		e1.setId(01);
+		e1.setName("Narendra");
+		System.out.println("ID:"+e1.getId()+"\nName:"+e1.getName());
+
+	}
+
+}
